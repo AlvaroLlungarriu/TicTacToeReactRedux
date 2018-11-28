619a1edc4bcd4688bd495e44aba2018d1f0dbf74
Alvaro Ll <alvaro.llungarriu@gmail.com>
2018-11-28 12:56:18 +0100

    Poniendo comentarios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Game from './components/Game';
+import Game from './components/Game';//Game es la clase principal y lo contiene todo
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));//Solo puede haber un ReactDOM, siempre ponemos la clase principal que en este caso es Game
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
